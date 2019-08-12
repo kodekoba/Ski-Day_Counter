ccd0533f454af2198a4a646cd769fcc626f5c5f9
Adriel Ordonez <Adriel_Ordonez@epam.com>
2019-08-12 13:06:35 -0700

    c 081219 1300 compose reducers for add ski day/ created remove ski day by date

--- src/index.js
+++ src/index.js
@@ -1,21 +1,31 @@
 import C from './constants'
-import { errors } from './store/reducers.js'
+import { allSkiDays } from './store/reducers.js'
 
 const state = [
-    'user not authorized ',
-    'server feed not found'
+    {
+        "resort": "Kirkwood",
+        "date": "2019-12-15",
+        "powder": true,
+        "backcountry": false
+    },
+    {
+        "resort": "Boreal",
+        "date": "2019-12-16",
+        "powder": false,
+        "backcountry": false
+    }
 ]
 
 const action = {
-    type: C.CLEAR_ERROR,
-    payload: 0 //index of error to be removed
+    type: C.REMOVE_DAY,
+    payload: "2016-12-15"
 }
 
-const nextState = errors(state, action)
+const nextState = allSkiDays(state, action)
 
 console.log(`
 
-    initial state: ${state}
+    initial state: ${JSON.stringify(state)}
     action: ${JSON.stringify(action)}
     next state: ${JSON.stringify(nextState)}
 
--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,12 +1,11 @@
 import C from '../constants'
 
-export const goal = (state=10, action) => {
+export const goal = (state=10, action) => { // remove braces for brevity?
     (action.type === C.SET_GOAL) ? parseInt(action.payload) : state // efficient ternary statement
 }
 
-export const skiDay = (state=null, action) => {
-    (action.type === C.ADD_DAY) ? parseInt(action.payload) : state
-}
+export const skiDay = (state=null, action) => // braces make this break when called in allSkiDays
+    (action.type === C.ADD_DAY) ? action.payload : state
 
 export const errors = (state=[], action) => {
 
@@ -24,4 +23,26 @@ export const errors = (state=[], action) => {
             return state
     }
 
+}
+
+export const allSkiDays = (state=[], action) => {
+
+    switch(action.type) {
+
+        case C.ADD_DAY :
+            const hasDate = state.some(skiDay => skiDay.date === action.payload.date)
+
+            return (hasDate) ? // ternary to prevent same day entries
+                state : // if true, does not add new entry
+            [           // otherwise adds new ski day
+                ...state,
+                skiDay(null, action)
+            ].sort((a,b) => new Date(b.date) - new Date(a.date))
+
+        case C.REMOVE_DAY :
+            return state.filter(skiDay => skiDay.date !== action.payload)
+
+        default:
+            return state
+    }
 }
