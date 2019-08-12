a590ba922681a6667543e85ef2b8352bcdaf3aa3
Adriel Ordonez <Adriel_Ordonez@epam.com>
2019-08-12 12:21:47 -0700

    c 081219 1220 add reducers for add error/ clear error

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -7,3 +7,21 @@ export const goal = (state=10, action) => {
 export const skiDay = (state=null, action) => {
     (action.type === C.ADD_DAY) ? parseInt(action.payload) : state
 }
+
+export const errors = (state=[], action) => {
+
+    switch(action.type) {
+        case C.ADD_ERROR :
+            return [
+                ...state, 
+                action.payload
+            ]
+        
+        case C.CLEAR_ERROR :
+            return state.filter((message, i) => i !== action.payload)
+
+        default:
+            return state
+    }
+
+}
